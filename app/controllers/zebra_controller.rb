c48591c2b89c7a184465e72fa26e19a8188fc322
Akin Campbell <54187807+akincampbell@users.noreply.github.com>
2023-09-25 21:51:40 +0000

    Add routes. Add controller. Add rock html file--no content

---
+++ app/controllers/zebra_controller.rb
@@ -0,0 +1,16 @@
+class zebraController < ApplicationController
+  def giraffe
+    @random_move = ["rock", "paper", "scissors"].sample
+    render({:template => game_templates/play_rock})
+  end
+
+  def elephant
+    @random_move = ["rock", "paper", "scissors"].sample
+    render({:template => game_templates/play_rock})
+  end
+
+  def lion
+    @random_move = ["rock", "paper", "scissors"].sample
+    render({:template => game_templates/play_rock})
+  end
+end
